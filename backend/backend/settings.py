03e62eca1f60f84f54e38a904f8adb80a4120453
eric-castillo05 <ericcastillo005@gmail.com>
2023-10-08 15:57:30 -0600

    security: Add .env file to store sensitive information

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wo!k4n#*$ao_h^z)sr9gt!94#)w%1zorax+7ee#hm=(7!tq#5t'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login',
     'redis',
+    'django.contrib.staticfiles',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -87,9 +88,9 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis-17982.c261.us-east-1-4.ec2.cloud.redislabs.com:17982/0',
+        'LOCATION': config('LOCATION'),
         'OPTIONS': {
-            'PASSWORD': '7jSsdnZ9wUrYAs1O2xODhSSr13q1U5LL',
+            'PASSWORD': config('PASSWORD'),
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
